f1ace92780f2646309180124893c2408bbda9253
anujpandey0212 <MS20BTECH11003@iith.ac.in>
2022-05-17 21:49:30 +0530

    landing page component added

--- client/src/components/landing_page/index.js
+++ client/src/components/landing_page/index.js
@@ -1,17 +1,18 @@
 import React from "react";
 import "./style.css";
 import rocket from "../../assets/rocket.png";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default  function LandingPpage(){
     return(
-       <div className="row landing_page">
+       <div className="row">
            <div className="col-md-6">
                 <h1>STARTUP</h1>
                 <h1>FAIR</h1>
                 <p>its not about ideas</p>
                 <p>its about making ideas happen</p>
            </div>
-           <div className>
+           <div className="col-md-6">
                 <img alt="rocket" src={rocket} height={100}></img>
            </div>
        </div>
