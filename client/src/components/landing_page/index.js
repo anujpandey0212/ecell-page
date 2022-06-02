75946a45ae986e6471626e49c157a45a1786bb5d
Siddarth Saha <siddarthsaha1@gmail.com>
2022-06-02 19:58:06 +0530

    Added more MUI components. Optimized for mobile. Combined the rocket img

--- client/src/components/landing_page/index.js
+++ client/src/components/landing_page/index.js
@@ -1,33 +1,138 @@
 import React from "react";
 import "./style.css";
-import rocket from "../../assets/rocket.png";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import vision from "../../assets/vision.png";
-import strategy from "../../assets/strategy.png";
-import marketing from "../../assets/marketing.png";
-import ideas from "../../assets/ideas.png";
+import { Grid, Typography, Button } from "@mui/material";
+import useTheme from "@mui/material/styles/useTheme";
+import { useMediaQuery } from "@mui/material";
 
-export default  function LandingPpage(){
-    return(
-       <div className="landing-page row" style={{marginRight:0}}>
-           <div className="column_our col-md-6" style={{padding:40}}>
-               <div style={{display:"inline-flex",float:"left"}} className="startup">
-                    <h1 className="font_style_1">START</h1>
-                    <h1 className="font_style_1" style={{color:"#13EEAC"}}>UP</h1>
-               </div>
-                
-                <h1 className="font_style_1">FAIR</h1>
-                <p className="font_style_2 ideas_first_line">IT'S NOT ABOUT THE IDEAS.</p>
-                <p className="font_style_2">IT'S ABOUT MAKING IDEAS HAPPEN.</p>
-               <button id="start_button" type="button" class="btn btn-primary">Start</button>
-           </div>
-           <div className="column_our col-md-6">
-                <img alt="vision" src={vision} className="four-images"></img>
-                <img alt="vision" src={strategy}  className="four-images"></img>
-                <img alt="rocket" src={rocket} className="rocket-image"></img>
-                <img alt="vision" src={marketing}  className="four-images"></img>
-                <img alt="vision" src={ideas}  className="four-images"></img>
-           </div>
-       </div>
-    )
+import rocket from "../../assets/rocket-full.png";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Box } from "@mui/system";
+
+/**
+ * The use of bootstrap has been replaced with material UI
+ *
+ */
+
+export default function LandingPpage() {
+  const theme = useTheme();
+  const matchesmd = useMediaQuery(theme.breakpoints.up("sm"));
+  const matcheslg = useMediaQuery(theme.breakpoints.up("md"));
+  const matchessm = useMediaQuery(theme.breakpoints.down("sm"));
+
+  // for the subtext
+  theme.typography.h2 = {
+    [theme.breakpoints.down("md")]: {
+      textAlign: "center",
+    },
+    [theme.breakpoints.down("sm")]: {
+      fontSize: "1rem",
+      textAlign: "center",
+    },
+  };
+  // for the STARTUP FRONT text
+  theme.typography.h1 = {
+    fontSize: "7rem",
+    [theme.breakpoints.down("md")]: {
+      fontSize: "5rem",
+      textAlign: "center",
+    },
+    [theme.breakpoints.down("sm")]: {
+      fontSize: "2.5rem",
+      textAlign: "center",
+    },
+  };
+
+  return (
+    <>
+      <Grid
+        container
+        sx={{
+          height: "100vh",
+          background:
+            "radial-gradient(50% 50% at 50% 60%, #181743 0%, #340C64 100%);",
+          pt: matcheslg ? 3 : matchesmd ? "6rem" : matchessm && "6rem",
+        }}
+      >
+        <Grid
+          item
+          xs={12}
+          md={8}
+          lg={6}
+          sx={{
+            display: matchesmd && "flex",
+            flexDirection: matchesmd && "column",
+            justifyContent: matchesmd ? "center" : "flex-start",
+            pl: matcheslg ? 9 : 0,
+            pt: matcheslg ? 7 : 0,
+          }}
+        >
+          <Box>
+            <Typography
+              variant="h1"
+              sx={{
+                position: "relative",
+                zIndex: 1,
+                color: "white",
+                fontWeight: "700",
+                fontFamily: "Poppins",
+                py: matchessm ? "1rem" : 0,
+              }}
+            >
+              START<span className="up">UP</span> <br />
+              FAIR
+            </Typography>
+          </Box>
+          <Box>
+            <Typography
+              variant="h2"
+              sx={{
+                position: "relative",
+                zIndex: 1,
+                fontWeight: 400,
+                color: "white",
+                fontSize: "1.8rem",
+                textAlign: matchesmd ? "left" : "center",
+              }}
+            >
+              IT’S NOT ABOUT IDEAS. <br />
+              IT’S ABOUT MAKING IDEAS HAPPEN.
+            </Typography>
+          </Box>
+          <Box
+            sx={{
+              width: "100%",
+              display: "flex",
+              justifyContent: matcheslg ? "flex-start" : "center",
+              mt: matcheslg ? 6 : 3,
+            }}
+          >
+            <Button variant="green" sx={{ fontSize: matchessm && "1rem" }}>
+              START
+            </Button>
+          </Box>
+        </Grid>
+        <Grid container item xs={12} md={4} lg={6}>
+          <Box
+            component="img"
+            src={rocket}
+            sx={{
+              zIndex: 0,
+              position: "absolute",
+              transform: matchessm ? "translateX(50%)" : "translateX(0)",
+              right: matchessm ? "50%" : 0,
+              bottom: 0,
+              filter:
+                useMediaQuery(theme.breakpoints.down("md")) &&
+                "brightness(75%)",
+              height: matcheslg
+                ? "31.25rem"
+                : matchesmd
+                ? "21.25rem"
+                : matchessm && "16.5rem",
+            }}
+          />
+        </Grid>
+      </Grid>
+    </>
+  );
 }
